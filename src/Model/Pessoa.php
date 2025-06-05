ee1699607f02064eca29f6d04d3442b39663741d
Jean <jeanribeiro1905@gmail.com>
2025-06-05 15:42:50 -0300

    Update Pessoa.php
    Adicionei o declare, namespace e troquei o private pelo protected para outras classes poderem acessar o atributo $nome.

--- src/Model/Pessoa.php
+++ src/Model/Pessoa.php
@@ -1,7 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
+namespace App\Model;
+
 class Pessoa
 {
+<<<<<<< Updated upstream
     private $nome;
     private $numero;
+=======
+    protected $nome;
+
+    public function __construct($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    public function getNome(): string {
+        return $this->nome;
+    }
+>>>>>>> Stashed changes
 }
